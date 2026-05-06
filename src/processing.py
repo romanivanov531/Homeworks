7b7d10fc2d3799190653c68fbcc6b917696b8216
Roman Ivanov <romanivanov5311@gmail.com>
2026-05-06 23:15:18 +0300

    add dockstrings, chek by flake8 mypy and formated bu black

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,14 @@
-def filter_by_state(list_: list, key: str = 'EXECUTED') -> list:
+def filter_by_state(list_: list, key: str = "EXECUTED") -> list:
+    """Функция, которая фильтрует список словарей по ключу state"""
     filtered_list = []
     for item in list_:
-        if item.get('state') == key:
+        if item.get("state") == key:
             filtered_list.append(item)
         else:
             continue
     return filtered_list
 
-def sort_by_date(list_: list, route:bool = True) -> list:
-    return sorted(list_, key=lambda x: x.get('date'), reverse=route)
+
+def sort_by_date(list_: list, route: bool = True) -> list:
+    """Функция, которая сортирует список словарей по ключу date"""
+    return sorted(list_, key=lambda x: x.get("date"), reverse=route)

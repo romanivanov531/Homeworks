a595e5aab125214f1b35b9f0fdf1bb7c12c61a38
Roman Ivanov <romanivanov5311@gmail.com>
2026-05-13 19:08:07 +0300

    fix by flake8 and mypy

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,7 @@
-def filter_by_state(list_: list, key: str = "EXECUTED") -> list:
-    """Функция, которая фильтрует список словарей по ключу state"""
+def filter_by_state(list_: list[dict], key: str = "EXECUTED") -> list[dict]:
+    """Функция, которая принимает список словарей и опционально значение для ключа state.
+Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+state соответствует указанному значению."""
     filtered_list = []
     for item in list_:
         if item.get("state") == key:
@@ -9,6 +11,7 @@ def filter_by_state(list_: list, key: str = "EXECUTED") -> list:
     return filtered_list
 
 
-def sort_by_date(list_: list, route: bool = True) -> list:
-    """Функция, которая сортирует список словарей по ключу date"""
-    return sorted(list_, key=lambda x: x.get("date"), reverse=route)
+def sort_by_date(list_: list[dict], route: bool = True) -> list[dict]:
+    """Функция, которая принимает список словарей и необязательный параметр, задающий порядок сортировки.
+    Возвращает список, отсортированный по ключу date """
+    return sorted(list_, key=lambda x: x["date"], reverse=route)

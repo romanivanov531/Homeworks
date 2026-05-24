22313baa81c1d5e91609e7704d8309063c5c662b
Roman Ivanov <romanivanov5311@gmail.com>
2026-05-24 16:30:30 +0300

    fix src/generators.py by mypy and flake8

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,7 @@
-def filter_by_currency(my_list: list[dict], key: str = 'USD'):
+from typing import Generator, Any, Dict, List
+
+
+def filter_by_currency(my_list: List[Dict[str, Any]], key: str = 'USD') -> Generator[Dict[str, Any]]:
     '''Функция принимает список транзакций в виде словарей и фильтрует по валюте.
     По умолчанию валюта USD.
     '''
@@ -7,20 +10,23 @@ def filter_by_currency(my_list: list[dict], key: str = 'USD'):
             yield item
 
 
-def transaction_descriptions(my_list: list[dict]):
+def transaction_descriptions(my_list: list[Dict[str, Any]]) -> Generator[str]:
     '''Функция принимает список транзакций в виде словарей и возвращает описание каждой операции по очереди.'''
     for a in my_list:
         description = a['description']
         yield description
 
 
-def card_number_generator(start: int, stop: int):
+def card_number_generator(start: int, stop: int) -> Generator:
     '''Генератор, который выдает номера карт в формате: ХХХХ ХХХХ ХХХХ ХХХХ.
     Генератор работает в диапазоне от 0000 0000 0000 0000 до 9999 9999 9999 9999.
     Для генерации необходимы граничные значения.
     '''
     if start < 1 or stop > 9999999999999999 or stop < start:
-        raise ValueError('ddd')
+        raise ValueError('Нарушено одно из правил:'
+                         'Начальное значение меньше 1'
+                         'Конечное значение больше 999999999999'
+                         'Начальное значение больше конечного')
     for number in range(start, stop + 1):
         new_number = f'{number:016d}'
         new_card = new_number[:4] + ' ' + new_number[5:9] + ' ' + new_number[9:13] + ' ' + new_number[12:]
